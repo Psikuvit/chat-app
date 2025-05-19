d813c912898fe2391414bf6ae725a35d8461007d
Psikuvit <thegengestar@gmail.com>
2025-05-19 22:36:54 +0100

    Fix #6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   images: {
-    domains: ['localhost', 'your-railway-app-url.com'],
+    domains: ['localhost', 'https://chatapp-a8f.up.railway.app/'],
   },
   env: {
     NEXT_PUBLIC_SOCKET_SERVER_URL: process.env.NEXT_PUBLIC_SOCKET_SERVER_URL,
